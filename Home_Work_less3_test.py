bc544499f248304ec7e75da06b500aa2b36290b8
sSPGs <xtspgtx@gmail.com>
2020-08-01 18:35:41 +0300

    two commit

---
+++ Home_Work_less3_test.py
@@ -0,0 +1,34 @@
+# value = str(input("Введите значение str"))
+# len_digit = int(input("Введите длину списка"))
+#
+# a = int(input("введите первое значение"))
+# b = int(input("введите второе значение"))
+# c = int(input("введите второе значение"))
+#
+# my_str = value if len(value) >= len_digit else value[a:b:c]
+#
+# print(my_str)
+# print("--------------------------------")
+#
+#
+#
+# value = str("123456789")
+#
+# my_str = value if len(value) >= 5 else value[2:5:2]
+#
+# print(my_str)
+# print("--------------------------------")
+
+#
+# value = "123456789"
+#
+# my_str = value if len(value) <= 5 else value[2:5:2]
+#
+# print(my_str[::-1])
+
+
+# my_str = "My name is Vova. I am a teatcher !"
+# for symbol in my_str:
+#     if not symbol.isupper() and symbol.isalpha():
+#         print(symbol)
+
