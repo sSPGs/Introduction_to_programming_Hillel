2dc1e5f445fab4a96674833af90522beb646009d
Vladimir Hrechuha <xtspgtx@gmail.com>
2020-08-04 11:20:43 +0300

    Homework_3_3_task_1+2_success.py

--- Homework_3_3.py
+++ Homework_3_3.py
@@ -8,13 +8,26 @@
 # bla
 # bla
 
+# my_str = "blablablablacar"
+# my_symbol = "bla"
+# count = my_str.count(my_symbol)
+# if count > 0:
+#     while count > 0:
+#         count = count - 1
+#         print(my_symbol)
+# else:
+#     print("Wrong!!!!")
 
-my_str = str("blablacar")
-my_symbol = str("bla")
+my_str = str(input("Введите строку"))
+my_symbol = str(input("Введите искомое в строке"))
 count = my_str.count(my_symbol)
-while count >= 0:
-    print(my_symbol)
-    count = count - 2
+if count > 0:
+    while count > 0:
+        count = count - 1
+        print(my_symbol)
+else:
+    print("Wrong!!!!")
+
 
 #####################################################
 
@@ -34,8 +47,8 @@ count = my_str.count(my_symbol)
 print(f"Совпадений: {count}")
 
 #####################################################
+
 # 3) У вас есть переменная my_str, тип - str. Напечатать ЧИСЛО сколько РАЗНЫХ символов встречается в my_str. Большая и маленькая буква считается как один символ. Пробелы, запятые и т.д. считаем тоже как символы.
 # Пример:  my_str="bla BLA car".
 # Вывод на экран:
 # 6
-
