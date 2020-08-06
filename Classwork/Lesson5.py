061caad27deef2b782c9976526927a02ce92e34c
Vladimir Hrechuha <xtspgtx@gmail.com>
2020-08-06 14:53:14 +0300

    change_directory_v.0.2

--- Classwork/Lesson5.py
+++ Classwork/Lesson5.py
@@ -1,130 +1,109 @@
-###########как можно красивее было сделать домашку#########################################
-########################################
-# my_str = str(input("Введите строку"))
-# my_symbol = str(input("Введите искомое в строке"))
-#
-# count = my_str.count(my_symbol)
-# result_str = f"{my_symbol}\n" * count
-# print(result_str.strip())
+"""
+1.
+Дана строка my_str и пустой список my_list.
+Заполнить my_list символами из my_str, которые стоят на четных местах в строке
+"""
 
-# print(f"{my_symbol}\n" * count + "\n")
-#############################################
-#############################################
-# my_str = "bla BLA car"
-# my_str = my_str.lower()
-#
-# unique_symbol =  ''
-#
-# for symb in my_str:
-#     if symb not in unique_symbol:
-#         unique_symbol += symb
-# print(len(unique_symbol), unique_symbol)
-#############################################
-#############################################
+my_str = "qwerty"
+# решение через приведение типов
+my_list = []
+my_list += list(my_str[::2])
+print(my_list)
+# решение через цикл
+my_list = []
+for symbol in my_str[::2]:
+    my_list.append(symbol)
+print(my_list)
 
+"""
+2.
+Дана строка my_str, список str_index целых чисел в диапазоне от 0 до длинны строки, пустой список my_list.
+Заполнить my_list символами из my_str, которые стоят на местах с индексами из str_index
+"""
+my_list = []
+my_str = "qwerty"
+str_index = [0, 5, 3, 3, 4]
+for index in str_index:
+    my_list.append(my_str[index])
+print(my_list)
 
+"""
+3.
+Даны списки my_list_1 и my_list_2.
+Создать список my_result в который поместить элементы из my_list_1 и my_list_2 через один.
+a) кол-во эл-тов одинаковое
+б) кол-во эл-тов разное
+"""
+my_list_1 = [10, 20, 30, 40]
+my_list_2 = [3, 4, 5, 6, 7]
+my_result = []
+if len(my_list_1) == len(my_list_2):
+    for index in range(len(my_list_1)):
+        my_result.append(my_list_1[index])
+        my_result.append(my_list_2[index])
+else:
+    if len(my_list_1) < len(my_list_2):  # проверяем, какой список короче.
+        my_list_1, my_list_2 = my_list_2, my_list_1 # меняем списки местами. Длиннее дожен быть my_list_1
+    for index in range(len(my_list_2)):
+        my_result.append(my_list_1[index])
+        my_result.append(my_list_2[index])
+    print(my_result)
+    print(my_list_1[len(my_list_2):])
+    # my_result += my_list_1[len(my_list_2):]
+    my_result.extend(my_list_1[len(my_list_2):]) # расширение одного списка элементами другого списка
+print(my_result)
+"""
+4.
+Даны списки my_list_1 и my_list_2.
+Создать список my_result в который поместить четные элементы из my_list_1 и нечетные элементы из my_list_2.
+5.
+Дано целое число. Определить сколько цифр в этом числе.
+"""
+number = 746527310
+print(len(str(number)))
+"""
+6.
+Дано целое число. Определить наибольшую цифру в этом числе.
+"""
+number = 746527310
+number = str(number)
+print(max(number))
+"""
+7.
+Дано целое число. Составить число с цифрами в обратном порядке.
+"""
+number = 746527310
+print(int(str(number)[::-1]))
+"""
+8.
+Дано целое число. Составить число с цифрами в порядке возрастания.
+"""
+number = 746527310
 
-############РАБОТА В КЛАCСЕ##########################
-# 1.
-# Дана строка my_str и пустой список my_list.
-# # Заполнить my_list символами из my_str, которые стоят на четных местах в строке
-#
-# my_str = 'sadasfsagsgs'
-# my_list = []
-# my_str = list(my_str)
-# a = my_str[::2] + my_list
-#
-# print(a)
-#
-#
-#
-# # 2.
-# # Дана строка my_str, список str_index целых чисел в диапазоне от 0 до длинны строки, пустой список my_list.
-# # Заполнить my_list символами из my_str, которые стоят на местах с индексами из str_index
-# #
-#
-#
-# str_index = [0, 2, 4, 5, 3, 5, 2, 2, 5, 3, 3]
-# my_str = 'qwerty'
-# my_list = []
-#
-# for index in str_index:
-#     my_list.append(my_str[index])
-# print(my_list)
-
-
-
-#
-# 3.
-# Даны списки my_list_1 и my_list_2.
-# Создать список my_result в который поместить элементы из my_list_1 и my_list_2 через один.
-
-#Генератор RANGE  for value in range
-
-# my_list_1 = [10, 20, 30]
-# my_list_2 = [3, 4, 5, 123, 12]
-# my_result = []
-#
-# if len(my_list_1) == len(my_list_2):
-#     for index in range(len(my_list_1)):
-#         my_result.append(my_list_1[index])
-#         my_result.append(my_list_2[index])
-#
-# else:
-#     if len(my_list_1) < len(my_list_2):
-#         my_list_1, my_list_2 = my_list_2, my_list_1
-#
-#     for index in range(len(my_list_2)):
-#         my_result.append(my_list_1[index])
-#         my_result.append(my_list_2[index])
-#     # my_result += my_list_1[len(my_list_2):]
-#     # my_result.extend()
-# print(my_result)
-#
-# # 4.
-# # Даны списки my_list_1 и my_list_2.
-# # Создать список my_result в который поместить четные элементы из my_list_1 и нечетные элементы из my_list_2.
-# #
-# # 5.
-# # Дано целое число. Определить сколько цифр в этом числе.
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# # 6.
-# # Дано целое число. Определить наибольшую цифру в этом числе.
-#
-#
-#
-#
-# my_int = int(12312412429142121521)
-# my_int = str(my_int)
-# print(max(my_int))
-#
-#
-# a = 3
-# b = 4
-# z = [1,2,3,4,5]
-# 7.
-# Дано целое число. Составить число с цифрами в обратном порядке.
-
-# number = "123sad456789"
-# print(max(number))
-#
-# print(int(str(number)))
-
+# сортировка списка
 my_list = [3, 5, -6]
-new_list = sorted(my_list) # внешняя сортировка
-# my_list.sort() # внутренняя сортировка
+my_list.sort()
+print(my_list)
+
+# сортировка копии списка
+new_list = sorted(my_list)
 print(my_list)
 print(new_list)
 
-# 8.
-# Дано целое число. Составить число с цифрами в порядке возрастания.
-# """
+# разбор ДЗ
+my_str = "blablacar"
+my_symbol = "bla"
+
+count = my_str.count(my_symbol)
+result_str = f"{my_symbol}\n" * count
+print(result_str.strip())
+
+
+my_str="bla BLA car"
+my_str = my_str.lower()
+unique_symbols = ''
+
+for symb in my_str:
+    if symb not in unique_symbols:
+        unique_symbols += symb
+print(len(unique_symbols), unique_symbols)
