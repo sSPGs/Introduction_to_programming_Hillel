41be900e77071041a84e5afd0ce9445462ef0c06
Vladimir <xtspgtx@gmail.com>
2020-08-08 13:59:32 +0300

    Classwork6_Генератор-списков_Множества.py

--- Classwork/Classwork6.py
+++ Classwork/Classwork6.py
@@ -36,7 +36,45 @@
 
 ####Множества (set)######
 
-str_ = 'qwety not ytrewq'
+# str_1 = 'qwety not ytrewq'
+# str_2 = 'srssdg'
+#
+# set_1 = set(str_1)
+# set_2 = set(str_2)
+#
+# print(set_1.intersection(set_2)) # Поиск общих символов в стороках - Новый объект
+# print(set_1.union(set_2)) # объединяет два множества в одно, только уникальное
+# set_1.update(set_2)# добавление множества в другое множества
+# new_set = set_1.union(set_2)
+# print(new_set)
+# my_set =set(str_)
+# print(len(my_set)) # Вывод уникальных символов в строке
+#
+# for value in my_set:
+#     print(value)
+# for value in my_set:
+#     print(value)
+#
+# print(list(str_))
+# print((set(str_))) # поиск уникальных элементов
 
-print(list(str_))
-print((set(str_))) # поиск уникальных элементов
+##############################################3#
+####Множества (set)######
+# my_list = [1, 2, 3, 4, 1, 3, 5, 5, 2]
+#
+# new_list = list(set(my_list)) # Перезапись списка - записует только уникальные значния
+# print(new_list)
+# ##############################################3#
+#
+# qwe = {1}
+# print(type(qwe))
+
+#######Задача: Вывести символ и его количество повторов#######################################3#
+
+
+my_str = 'afggllhflyuasfyldfayfljv lavalafshlfscfcaflgsfcadshvas fgcahgecf.gdsfc jgsafc.j'
+
+set_my_str = set(my_str) # SET
+
+for symbol in set_my_str: # перебераем символы и выводим их количество по строчно
+    print(f'{symbol}: {my_str.count(symbol)}')
