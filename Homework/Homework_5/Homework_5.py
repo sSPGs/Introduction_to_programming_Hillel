270b9e230e2fb950dce2302f75178f3b1e0d725b
sSPGs <xtspgtx@gmail.com>
2020-08-10 00:32:07 +0300

    Homework_5_complete_task_1-and-4.py

--- Homework/Homework_5/Homework_5.py
+++ Homework/Homework_5/Homework_5.py
@@ -1,10 +1,10 @@
 # 1. Дано целое число (int). Определить сколько нулей в этом числе.
 
 ######################################################
-#
-# numb = 1021284010000000122310355653043015134
-# numb = str(numb)
-# print(numb.count("0"))
+
+numb = 1021284010000000122310355653043015134
+numb = str(numb)
+print(numb.count("0"))
 ######################################################
 
 
@@ -12,46 +12,11 @@
 # Если my_list [1,2,3,4], то new_list [2,3,4,1]
 ####################################################
 
-
-
 my_list = [1, 2, 3, 4]
-
-new_list = my_list.copy()
-# my_list = str(my_list)
-index_list = len(my_list)
-
-for index_list in new_list:
-    index_list -= 1
-    if index_list % 2 == 0:
-        my_list = list(my_list)
-        index_4et = index_list
-        print(f'''Четные:{index_4et}''')
-
-    elif index_list % 2 != 0:
-        index_He4et = index_list
-        print(f'''Нечетные:{index_He4et}''')
-
-# print(index_a)
-
-# print(f'''{new_list[0:-1:-1]}''')
-
-    # new_list.append(index_list)
-
-
-# new_list = new_list[2::-1]
-# # new_list = my_list[::-1]
-# len_my_list = len(my_list)
-# my_list.index(len_my_list)
-# # new_list = new_list[2::-1]
-# new_list.append(my_list[-4])
-
-
-
-
-
-
-
-
+new_list = my_list[:]
+del new_list[0]
+index_zero_my_list = my_list.copy()
+new_list.append(my_list[0])
+print(new_list)
 
 ####################################################
-
