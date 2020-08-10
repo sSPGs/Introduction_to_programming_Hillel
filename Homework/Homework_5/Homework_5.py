1afc42f24b68b459605797c5c3792d5ffe5d6a54
Vladimir Hrechuha <xtspgtx@gmail.com>
2020-08-10 16:59:38 +0300

    Homework_5_task-1,4,start6.py

--- Homework/Homework_5/Homework_5.py
+++ Homework/Homework_5/Homework_5.py
@@ -1,24 +1,22 @@
-# 1. Дано целое число (int). Определить сколько нулей в этом числе.
-
+#1
 ######################################################
-
 numb = 1021284010000000122310355653043015134
+print(f"1. Дано целое число {numb}. Определить сколько нулей в этом числе.")
 numb = str(numb)
-print(numb.count("0"))
+print(f'В числе {numb}\nКол-во НУЛЕЙ: {numb.count("0")}')
 print("######################################################")
 ######################################################
-
-
-#4. Дан список my_list. Создать список new_list у которого первый элемент из my_list стоит на последнем месте.
-# Если my_list [1,2,3,4], то new_list [2,3,4,1]
+#4
 ####################################################
-
+print("4.Дан список my_list. Создать список new_list у которого первый элемент из my_list стоит на последнем месте.\nЕсли my_list [1,2,3,4], то new_list [2,3,4,1]")
 my_list = [1, 2, 3, 4]
 new_list = my_list[:]
 del new_list[0]
 new_list.append(my_list[0])
 print(new_list)
 print("######################################################")
+####################################################
+#6
 ####################################################
 '''6. Дана строка в которой есть числа (разделяются пробелами). 
 Например "43 больше чем 34, но меньше чем 56". Найти сумму ВСЕХ чисел в этой строке.'''
@@ -32,7 +30,7 @@ box_forward_string_to_int_step = ''
 
 for iter_str_and_int in my_str:
     if iter_str_and_int in "1234567890":
-        box_int.append(iter_str_and_int)
+        box_int.append((iter_str_and_int))
     else:
         box_str.append(iter_str_and_int)
 
@@ -47,7 +45,7 @@ for itt_box_forward_int in box_int_sum:
     box_int_sum = box_int_sum.append(itt_box_forward_int)
     # print(box_int_sum)
 print(f"ТИП box_forward_string_to_int_step {type(box_forward_string_to_int_step)}")
-
+print(box_forward_string_to_int_step)
 
 print(type(box_int_sum))
 
