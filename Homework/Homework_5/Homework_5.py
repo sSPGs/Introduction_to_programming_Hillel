5d6b6534e527fca3cc20c7a73c66b9c66d099031
Vladimir Hrechuha <xtspgtx@gmail.com>
2020-08-14 16:21:16 +0300

    13.08.20bv

--- Homework/Homework_5/Homework_5.py
+++ Homework/Homework_5/Homework_5.py
@@ -1,30 +1,100 @@
+
+
 #1
+
 
 '''"1. Дано целое число (int). Определить сколько нулей в этом числе.'''
 #####################################################
 
-numb = 1021284010000000122310355653043015134
-
-sum_numb = str(numb)
-sum_total = sum_numb.count("0")
-
-print(f'В числе {numb}\nКол-во НУЛЕЙ: {sum_total}')
+# numb = 1021284010000000122310355653043015134
+#
+# sum_numb = str(numb)
+# sum_total = sum_numb.count("0")
+#
+# print(f'В числе {numb}\nКол-во НУЛЕЙ: {sum_total}')
 print("######################################################")
 
 ####################################################
+
+'''2. Дано целое число (int). Определить сколько нулей в конце этого числа.'''
+####################################################
+
+numb_int = 10203000
+numb_str = str(numb_int)
+
+# numb_str = numb_str[::-1]
+
+len_numb_str = len(numb_str)
+idx_len_numb_str = len_numb_str - 1
+count_numb_str = 0
+print(numb_str)
+
+# while idx_len_numb_str >= 0:
+#     idx_len_numb_str -= idx_len_numb_str
+#     if numb_str[idx_len_numb_str] == '0':
+#         numb_str.count('0')
+#         print(numb_str)
+#     else:
+#         break
+#
+# print(numb_str.count('0'))
+
+####################################################
+
+'''3. Даны списки my_list_1 и my_list_2. Создать список my_result в который вначале поместить четные элементы 
+из my_list_1 и потом нечетные элементы из my_list_2.'''
+####################################################
+
+# my_list_1 = [2, 6, 112, 12, 113, 1235]
+# my_list_2 = [5, 121, 5135, 3624, 13153, 135]
+# my_result = []
+#
+# for idx, val in enumerate(my_list_1):
+#     if idx % 2:
+#         my_result.append(val)
+#     else:
+#         pass
+# for idx, val in enumerate(my_list_2):
+#     if idx % 2:
+#         pass
+#     else:
+#         my_result.append(val)
+# print(my_result)
+#
+#         ####################################################
+
 
 
 '''4.Дан список my_list. Создать список new_list у которого первый элемент из my_list стоит на последнем месте.
 Если my_list [1,2,3,4], то new_list [2,3,4,1]'''
 ####################################################
 
-my_list = [1, 2, 3, 4]
-new_list = my_list
-new_list.append(new_list.pop(0))
-print(new_list)
-print("######################################################")
+# my_list = [1, 2, 3, 4]
+# new_list = my_list
+# new_list.append(new_list.pop(0))
+# print(new_list)
+# print("######################################################")
 
 ####################################################
+
+'''5.Дан список my_list. В ЭТОМ списке первый элемент переставить на последнее место. 
+[1,2,3,4] -> [2,3,4,1]. Пересоздавать список нельзя!'''
+
+####################################################
+
+# my_list = [1, 2, 3, 4]
+# # my_list.insert(0, 2)
+# my_list.insert(0, my_list.pop(1))
+# my_list.insert(1, my_list.pop(2))
+# my_list.insert(2, my_list.pop(3))
+# print(my_list)
+
+
+
+
+####################################################
+
+
 
 
 
@@ -32,102 +102,41 @@ print("######################################################")
 Например "43 больше чем 34, но меньше чем 56". Найти сумму ВСЕХ чисел в этой строке.'''
 ####################################################
 
-my_str = "43 больше чем 34, но меньше чем 56"
-my_str.split()
-box_int = []
-
-'''Цикл for iter_str_and_int in my_str:  '''
-for iter_str_and_int in my_str:
-    if iter_str_and_int in "1234567890":
-        iter_str_and_int = int(iter_str_and_int)
-        box_int.append((iter_str_and_int)) ## Выбераем из строки  цифры и помещаем в список box_int
-    else:
-        pass
-
-a, b = box_int[0], box_int[1]
-a = str(a)
-b = str(b)
-ab = a+b
-ab = int(ab)
-
-c, d = box_int[2], box_int[3]
-c = str(c)
-d = str(d)
-cd = c+d
-cd = int(cd)
-
-e, f = box_int[4], box_int[5]
-e = str(e)
-f = str(f)
-ef = e + f
-ef = int(ef)
-
-res = (ab + cd + ef)
-print(res)
+# my_str = "43 больше чем 34, но меньше чем 56"
+# my_str.split()
+# box_int = []
+#
+# '''Цикл for iter_str_and_int in my_str:  '''
+# for iter_str_and_int in my_str:
+#     if iter_str_and_int in "1234567890":
+#         iter_str_and_int = int(iter_str_and_int)
+#         box_int.append((iter_str_and_int)) ## Выбераем из строки  цифры и помещаем в список box_int
+#     else:
+#         pass
+#
+# a, b = box_int[0], box_int[1]
+# a = str(a)
+# b = str(b)
+# ab = a+b
+# ab = int(ab)
+#
+# c, d = box_int[2], box_int[3]
+# c = str(c)
+# d = str(d)
+# cd = c+d
+# cd = int(cd)
+#
+# e, f = box_int[4], box_int[5]
+# e = str(e)
+# f = str(f)
+# ef = e + f
+# ef = int(ef)
+#
+# res = (ab + cd + ef)
+# print(res)
 
 print("######################################################")
 ####################################################
-
-
-
-'''2. Дано целое число (int). Определить сколько нулей в конце этого числа.'''
-####################################################
-
-
-
-
-
-
-
-
-
-
-####################################################
-
-
-
-'''3. Даны списки my_list_1 и my_list_2. Создать список my_result в который вначале поместить четные элементы 
-из my_list_1 и потом нечетные элементы из my_list_2.'''
-####################################################
-
-my_list_1 = [2, 6, 112, 12, 113, 1235]
-my_list_2 = [5, 121, 5135, 3624, 13153, 135]
-my_result = []
-
-for idx, val in enumerate(my_list_1):
-    if idx % 2:
-        my_result.append(val)
-    else:
-        pass
-for idx, val in enumerate(my_list_2):
-    if idx % 2:
-        pass
-    else:
-        my_result.append(val)
-print(my_result)
-
-        ####################################################
-
-
-'''5.Дан список my_list. В ЭТОМ списке первый элемент переставить на последнее место. 
-[1,2,3,4] -> [2,3,4,1]. Пересоздавать список нельзя!'''
-
-####################################################
-
-my_list = [1, 2, 3, 4]
-# my_list.insert(0, 2)
-my_list.insert(0, my_list.pop(1))
-my_list.insert(1, my_list.pop(2))
-my_list.insert(2, my_list.pop(3))
-print(my_list)
-
-
-
-
-####################################################
-
-
-
 
 
 '''7. Дана строка my_str. Разделите эту строку на пары из двух символов и поместите эти пары в список.
@@ -153,13 +162,10 @@ my_str = "My_long str", l_limit = "o", r_limit = "t" -> sub_str = "ng s"'''
 
 ####################################################
 
-my_str = "My_long str"
-
-sub_str = my_str[5:9]
-print(sub_str)
-
-
-
+# my_str = "My_long str"
+#
+# sub_str = my_str[5:9]
+# print(sub_str)
 
 
 
@@ -171,11 +177,6 @@ print(sub_str)
 Причем l_limit левее чем r_limit. В переменную sub_str поместить НАИБОЛЬШУЮ часть строки между этими символами. 
 my_str = "My long string", l_limit = "o", r_limit = "g" -> sub_str = "ng strin".'''
 ####################################################
-
-
-
-
-
 
 
 
--- Homework/Homework_6/Homework_6.py
+++ Homework/Homework_6/Homework_6.py
@@ -1,6 +1,6 @@
-import re
 
-'''1. Дан список строк my_list. Создать новый список в который поместить
+'''                            +
+1. Дан список строк my_list. Создать новый список в который поместить
 элементы из my_list по следующему правилу:
 Если строка стоит на нечетном месте в my_list, то ее заменить на
 перевернутую строку. "qwe" на "ewq".
@@ -11,7 +11,6 @@ import re
 # my_list = ["5 Horse", 123, 5135, 3624, "13 _ 153", 135]
 #
 # new_my_list = []
-# # new_my_list_4et = []
 # new_my_list_Ne4et = []
 # box_ne4 = []
 #
@@ -22,7 +21,7 @@ import re
 #         if type(val) == int:
 #
 #             val = str(val)
-#             # new_my_list_Ne4et.append(val)
+#             new_my_list_Ne4et.append(val)
 #             ne4 = new_my_list_Ne4et
 #
 #         else:
@@ -33,7 +32,7 @@ import re
 # for ind_ne4, val_ne4 in enumerate(new_my_list_Ne4et):
 #     val_ne4 = val_ne4[::-1]
 #     box_ne4.append(val_ne4)
-#     print(f"ТИП VAL_NE4:::> {type(val_ne4)},{val_ne4}")
+#     # print(f"ТИП VAL_NE4:::> {type(val_ne4)},{val_ne4}")
 #     for i, val in enumerate(val_ne4):
 #         val = list(val)
 # a = new_my_list
@@ -47,12 +46,9 @@ import re
 элементы из my_list у которых первый символ - буква "a".
 '''
 #######################################################################
-#
+
 my_list = ["baasasdasfaw", "askasknfmncxnb.a\e", "asfasfcxxczzzzz1`1112", "sasfadasf", "eet2344", "asdvxczzasfa", "asa","a1", "s1", "b1", "b2", "basd21", "as22211sv!", "a_a", "as1", "_a", "aaa2222"]
-# for ind_list, value_list in enumerate(my_list):
-my_list.sort()
-print(my_list)
-print(my_list.count("a"))
+
 
 #######################################################################
 '''
@@ -60,7 +56,21 @@ print(my_list.count("a"))
 элементы из my_list в которых есть символ - буква "a" на любом месте.
 '''
 #######################################################################
-# my_list = ["baasasdasfaw", "skasknfmncxnb.a\e", "asfasfcxxczzzzz1`1112", "sasfadasf", "eet2344", "sdvxczzasfa", "asa","a1", "s1", "b1", "b2", "basd21", "s22a211sv!", "a_a", "as1", "_a", "aaa2222"]
+
+my_list = ["12as12", "as12sa", "sa12sa", "a12a", "12hfdf", "122ssss",]
+a_list = []
+
+for key, val in enumerate(my_list):
+    if val in my_list.index([0]):
+        if val in my_list:
+            a_list.append(val)
+        else:
+            pass
+print(a_list)
+
+
+
+
 
 #######################################################################
 '''
@@ -100,12 +110,16 @@ else:
 # print(res)
 
 #######################################################################
-'''
+'''                                  +
 6. Даны две строки, вывести те символы, которые есть в обеих строках.
 '''
 #######################################################################
-# my_list_1 = [121, 'fjkjk', 3321, '300dsfgsd']
-# my_list_2 = [1421, 'fFFFAAsd', 21, '3a0dd']
+
+# my_str_1 = "dfgvbhnjmkk,./852611as4548654a21448641864564seasd213231321151!!!56"
+# my_str_2 = "dfgvbhnjmkk,.asassd55454541231212312afdsanjhasd41864564se56!"
+# sum_my_str = my_str_1 + my_str_2
+# print(set(sum_my_str))
+
 
 #######################################################################
 '''
@@ -116,7 +130,7 @@ else:
 
 
 #######################################################################
-'''
+'''                                          +
 8. Описать с помощью словаря следующую структуру для конкретного человека (можно придумать):
 Фамилия
 Имя
@@ -148,8 +162,7 @@ else:
 # print(person)
 
 #######################################################################
-'''
-
+'''                                          +
 9. Описать с помощью словаря следующую структуру (рецепт ненастоящего торта, придумать и указать граммы для продуктов):
 Составляющие
     Коржи
@@ -189,6 +202,6 @@ else:
 #     }
 # }
 #
-# print(dict_tort["Глазурь"])
+# print(dict_tort)
 
 #######################################################################
