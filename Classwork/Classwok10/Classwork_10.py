bca97fdc5753af6d968c329d50b4d0be3e49414c
Vladimir <xtspgtx@gmail.com>
2020-08-22 14:20:01 +0300

    22 august

--- Classwork/Classwok10/Classwork_10.py
+++ Classwork/Classwok10/Classwork_10.py
@@ -2,6 +2,17 @@
 
 import json
 import csv
+def reader_txt(filename_with_path):
+    data = ''
+    with open(filename_with_path, "r") as txt_file:
+        data = txt_file.read()
+    return data
+
+def reader_json(filename_with_path):
+    pass
+
+def reader_csv(filename_with_path):
+    pass
 
 def file_reader(filename_with_path):
     ext = filename_with_path.rsplit(".", 1)[-1]
